f7ef67ce3d0b25dae71804294697de5c82a61d7e
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-02 15:54:58 -0300

    feat: add single task page

--- src/pages/tasks/[id].vue
+++ src/pages/tasks/[id].vue
@@ -1,5 +1,88 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { taskQuery } from '@/utils/supabaseQueries'
+import type { Task } from '@/utils/supabaseQueries'
+
+const route = useRoute()
+const task = ref<Task | null>(null)
+
+watchEffect(() => {
+  usePageStore().pageData.title = `Task: ${task.value?.name || ''}`
+})
+
+const getTask = async () => {
+  const { data, error } = await taskQuery(Number(route.params.id))
+  task.value = data
+}
+
+await getTask()
+</script>
 
 <template>
-  <div>Task!</div>
+  <Table v-if="task">
+    <TableRow>
+      <TableHead>Name</TableHead>
+      <TableCell>{{ task.name }}</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead>Description</TableHead>
+      <TableCell>{{ task.description }}</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead>Assignee</TableHead>
+      <TableCell>Lorem ipsum</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead>Project</TableHead>
+      <TableCell>{{ task.projects?.name }}</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead>Status</TableHead>
+      <TableCell>{{ task.status }}</TableCell>
+    </TableRow>
+    <TableRow>
+      <TableHead>Collaborators</TableHead>
+      <TableCell>
+        <div class="flex">
+          <Avatar
+            class="-mr-4 border border-primary transition-transform hover:scale-110"
+            v-for="collaborator in task.collaborators"
+            :key="collaborator"
+          >
+            <RouterLink class="flex h-full w-full items-center justify-center" to="">
+              <AvatarImage src="" alt="" />
+              <AvatarFallback>?</AvatarFallback>
+            </RouterLink>
+          </Avatar>
+        </div>
+      </TableCell>
+    </TableRow>
+    <TableRow class="hover:bg-transparent">
+      <TableHead class="pt-4 align-top">Comments</TableHead>
+
+      <TableCell>
+        Comments cards goes in here..
+
+        <div class="my-2 flex flex-col justify-between rounded-md bg-muted p-3">
+          <textarea
+            placeholder="Add your comment.."
+            class="prose-sm prose dark:prose-invert w-full max-w-full overflow-y-auto rounded border border-muted bg-background p-3 hover:border-muted"
+          >
+          </textarea>
+          <div class="mt-3 flex justify-between">
+            <Button> Comment </Button>
+            <div class="flex gap-4">
+              <button variant="ghost" @click.prevent>
+                <iconify-icon icon="lucide:paperclip"></iconify-icon>
+                <span class="sr-only">Attach file</span>
+              </button>
+              <button variant="ghost" @click.prevent>
+                <iconify-icon icon="lucide:image-up"></iconify-icon>
+                <span class="sr-only">Upload image</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </TableCell>
+    </TableRow>
+  </Table>
 </template>
--- src/utils/supabaseQueries.ts
+++ src/utils/supabaseQueries.ts
@@ -10,3 +10,6 @@ export type Projects = QueryData<typeof projectsQuery>
 export const projectQuery = (slug: string) =>
   supabase.from('projects').select('*, tasks ( id, name, status, due_date )').eq('slug', slug).single()
 export type Project = QueryData<ReturnType<typeof projectQuery>>
+
+export const taskQuery = (id: number) => supabase.from('tasks').select('*, projects( id, name )').eq('id', id).single()
+export type Task = QueryData<ReturnType<typeof taskQuery>>
