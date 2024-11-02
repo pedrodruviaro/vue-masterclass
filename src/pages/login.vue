80b27c317f8680cb5ad487b59f1b3c545a3b785c
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-02 17:31:13 -0300

    feat: add auth pages layout and components

---
+++ src/pages/login.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mx-auto -mt-20 flex min-h-[90vh] w-full items-center justify-center p-10 text-center">
+    <Card class="mx-auto w-full max-w-sm">
+      <CardHeader>
+        <CardTitle class="text-2xl"> Login </CardTitle>
+        <CardDescription> Login to your account </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div class="mb-4 flex flex-col items-center justify-center gap-4">
+          <Button variant="outline" class="w-full"> Register with Google </Button>
+          <Separator label="Or" />
+        </div>
+
+        <form class="grid gap-4">
+          <div class="grid gap-2">
+            <Label id="email" class="text-left">Email</Label>
+            <Input type="email" placeholder="johndoe19@example.com" required />
+          </div>
+          <div class="grid gap-2">
+            <div class="flex items-center">
+              <Label id="password">Password</Label>
+              <a href="#" class="ml-auto inline-block text-xs underline"> Forgot your password? </a>
+            </div>
+            <Input id="password" type="password" autocomplete required />
+          </div>
+          <Button type="submit" class="w-full"> Login </Button>
+        </form>
+        <div class="mt-4 text-center text-sm">
+          Don't have an account?
+          <RouterLink to="/register" class="underline"> Register </RouterLink>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+</template>
---
+++ src/pages/register.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts"></script>
+
+<script setup lang="ts"></script>
+
+<template>
+  <div class="mx-auto -mt-10 flex h-full min-h-[90vh] w-full items-center justify-center p-10 text-center">
+    <Card class="mx-auto h-full w-full max-w-sm">
+      <CardHeader>
+        <CardTitle class="text-2xl"> Register </CardTitle>
+        <CardDescription> Create a new account </CardDescription>
+      </CardHeader>
+      <CardContent>
+        <div class="mb-4 flex flex-col items-center justify-center gap-4">
+          <Button variant="outline" class="w-full"> Register with Google </Button>
+          <Separator label="Or" />
+        </div>
+        <form class="grid gap-4">
+          <div class="grid gap-2">
+            <Label id="username" class="text-left">Username</Label>
+            <Input id="username" type="text" placeholder="johndoe19" required />
+          </div>
+          <div class="flex flex-col justify-between gap-4 sm:flex-row">
+            <div class="grid gap-2">
+              <Label id="first_name" class="text-left">First Name</Label>
+              <Input id="first_name" type="text" placeholder="John" required />
+            </div>
+            <div class="grid gap-2">
+              <Label id="last_name" class="text-left">Last Name</Label>
+              <Input id="last_name" type="text" placeholder="Doe" required />
+            </div>
+          </div>
+          <div class="grid gap-2">
+            <Label id="email" class="text-left">Email</Label>
+            <Input id="email" type="email" placeholder="johndoe19@example.com" required />
+          </div>
+
+          <div class="grid gap-2">
+            <Label id="password" class="text-left">Password</Label>
+            <Input id="password" type="password" placeholder="*****" autocomplete required />
+          </div>
+
+          <div class="grid gap-2">
+            <Label id="confirm_password" class="text-left">Confirm Password</Label>
+            <Input id="confirm_password" type="password" placeholder="*****" autocomplete required />
+          </div>
+          <Button type="submit" class="w-full"> Register </Button>
+          <!-- <Button variant="outline" class="w-full"> Login with Google </Button> -->
+        </form>
+        <div class="mt-4 text-center text-sm">
+          Already have an account?
+          <RouterLink to="/login" class="underline"> Login </RouterLink>
+        </div>
+      </CardContent>
+    </Card>
+  </div>
+</template>
