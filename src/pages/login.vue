268b7c94508945a78b1a26a95f56061725fe477b
pedrodruviaro <pedrodruviaro@gmail.com>
2024-11-02 18:21:34 -0300

    feat: add auth actions and form data

--- src/pages/login.vue
+++ src/pages/login.vue
@@ -1,4 +1,19 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { login } from '@/utils/supabaseAuth'
+
+const formData = ref({
+  email: '',
+  password: '',
+})
+
+const router = useRouter()
+
+const signIn = async () => {
+  const success = await login(formData.value)
+  if (success) router.push('/')
+}
+</script>
 
 <template>
   <div class="mx-auto -mt-20 flex min-h-[90vh] w-full items-center justify-center p-10 text-center">
@@ -13,17 +28,17 @@
           <Separator label="Or" />
         </div>
 
-        <form class="grid gap-4">
+        <form class="grid gap-4" @submit.prevent="signIn">
           <div class="grid gap-2">
             <Label id="email" class="text-left">Email</Label>
-            <Input type="email" placeholder="johndoe19@example.com" required />
+            <Input type="email" placeholder="johndoe19@example.com" required v-model="formData.email" />
           </div>
           <div class="grid gap-2">
             <div class="flex items-center">
               <Label id="password">Password</Label>
               <a href="#" class="ml-auto inline-block text-xs underline"> Forgot your password? </a>
             </div>
-            <Input id="password" type="password" autocomplete required />
+            <Input id="password" type="password" autocomplete required v-model="formData.password" />
           </div>
           <Button type="submit" class="w-full"> Login </Button>
         </form>
--- src/pages/register.vue
+++ src/pages/register.vue
@@ -1,6 +1,23 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { register } from '@/utils/supabaseAuth'
 
-<script setup lang="ts"></script>
+const formData = ref({
+  username: '',
+  firstName: '',
+  lastName: '',
+  email: '',
+  password: '',
+  confirmPassword: '',
+})
+
+const router = useRouter()
+
+const signUp = async () => {
+  const success = await register(formData.value)
+  if (success) router.push('/')
+}
+</script>
 
 <template>
   <div class="mx-auto -mt-10 flex h-full min-h-[90vh] w-full items-center justify-center p-10 text-center">
@@ -14,36 +31,50 @@
           <Button variant="outline" class="w-full"> Register with Google </Button>
           <Separator label="Or" />
         </div>
-        <form class="grid gap-4">
+        <form class="grid gap-4" @submit.prevent="signUp">
           <div class="grid gap-2">
             <Label id="username" class="text-left">Username</Label>
-            <Input id="username" type="text" placeholder="johndoe19" required />
+            <Input id="username" type="text" placeholder="johndoe19" required v-model="formData.username" />
           </div>
           <div class="flex flex-col justify-between gap-4 sm:flex-row">
             <div class="grid gap-2">
               <Label id="first_name" class="text-left">First Name</Label>
-              <Input id="first_name" type="text" placeholder="John" required />
+              <Input id="first_name" type="text" placeholder="John" required v-model="formData.firstName" />
             </div>
             <div class="grid gap-2">
               <Label id="last_name" class="text-left">Last Name</Label>
-              <Input id="last_name" type="text" placeholder="Doe" required />
+              <Input id="last_name" type="text" placeholder="Doe" required v-model="formData.lastName" />
             </div>
           </div>
           <div class="grid gap-2">
             <Label id="email" class="text-left">Email</Label>
-            <Input id="email" type="email" placeholder="johndoe19@example.com" required />
+            <Input id="email" type="email" placeholder="johndoe19@example.com" required v-model="formData.email" />
           </div>
 
           <div class="grid gap-2">
             <Label id="password" class="text-left">Password</Label>
-            <Input id="password" type="password" placeholder="*****" autocomplete required />
+            <Input
+              id="password"
+              type="password"
+              placeholder="*****"
+              autocomplete
+              required
+              v-model="formData.password"
+            />
           </div>
 
           <div class="grid gap-2">
             <Label id="confirm_password" class="text-left">Confirm Password</Label>
-            <Input id="confirm_password" type="password" placeholder="*****" autocomplete required />
+            <Input
+              id="confirm_password"
+              type="password"
+              placeholder="*****"
+              autocomplete
+              required
+              v-model="formData.confirmPassword"
+            />
           </div>
-          <Button type="submit" class="w-full"> Register </Button>
+          <Button type="submit" class="w-full">Register</Button>
           <!-- <Button variant="outline" class="w-full"> Login with Google </Button> -->
         </form>
         <div class="mt-4 text-center text-sm">
